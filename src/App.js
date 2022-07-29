e901d8aa386a46d09a4e748acb1e168c20508da7
NikhilMasurkar <101415426+NikhilMasurkar@users.noreply.github.com>
2022-07-30 01:37:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,47 @@ import Cart from "./Cart";
 import Navbar from "./Navbar";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// const page_number = 1;
+
 function App() {
   const [products, setProducts] = useState([]);
-  // const [page , setPage]= useState(page_number)
 
-  useEffect(() => {
-    async function getData() {
-      if (products.length === 0) {
+  async function getData() {
+    if (products.length === 0) {
+      const resp = await fetch("https://fakestoreapi.com/products");
+      const data = await resp.json();
+
+      setProducts(data);
+    }
+  }
+
+  window.onscroll = function () {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      (async () => {
         const resp = await fetch("https://fakestoreapi.com/products");
         const data = await resp.json();
-        setProducts(data);
-      }
+        let newarr = [...products, ...data];
+        setProducts(newarr);
+      })();
     }
+  };
+
+  useEffect(() => {
     getData();
-  });
+  }, []);
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home products={products} />} />
-          <Route path="/:id" element={<Cart products={products} />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home products={products} />} />
+        <Route path="/Cart:id" element={<Cart products={products} />} />
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
